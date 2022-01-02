8f38b8282fc7383feee595f1ce097659391713eb
abe codeman <abecodeman@gmail.com>
2022-01-02 12:10:13 +0600

    1st

---
+++ database/migrations/2022_01_02_052540_create_contacts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id()->autoIncrements();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('surname');
+            $table->string('email');
+            $table->string("work")->nullable();
+            $table->string('address')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
